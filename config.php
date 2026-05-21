5fba235d0ca90e6ce039f94d63c3e909adc1292e
UsageBasedLingLab <ublab@uoregon.edu>
2026-05-21 07:51:53 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,38 +1,40 @@
-<?
-	///////////////////////////////////////////////////////
-	// Online Score Script
-	// Jeff Vance 
-	// Version 1.3
-	// Files and sources can be found at www.flyinvinteractive.com
-	//////////////////////////////////////////////////////
-
+@@ -1,31 +1,10 @@
+<?php
 	// You need to fill in this data from your own mySQL server
 	
 	// Your host -- for example localhost or mysql.server.com
-	$host = 'sql104.byethost7.com';
+	$host = 'shortline.proxy.rlwy.net';
 	
 	// Your user name for mySQL
-	$user = 'b7_41623426';
+	$user = 'root';
 	
 	// Your password for mySQL
-    $pass = '5ZPJdnQGWBWHi$6';
+    $pass = 'zJECPcanVgwbtvlERXEmrtXbqgcKjXhB';
 	
 	// Your database name for mySQL
-	$dbname= 'b7_41623426_BierPierPlayerData';
+	$dbname= 'railway';
 
 	// ATTENTION
 	// This is your secret key - Needs to be the same as the secret key in your game
 	// You can change this but remember to change it in your game.
 	// This is used to help secure the score and produce MD5 hashes
-    $secret_key = "this is secret";
+    $secret_key = "dragonfruit42";
 	
 	// Your table name for mySQL
 	// You can change this is you wish
-	$tname= 'player data';
+	$tname= 'scores';
 	
 	// Number of scores to save for each gameid
 	// Feel free to change this but the example file only lists 10 scores
 	// You would need to code this
-	$score_number = '100';
+	$score_number = '10';
 	
+$host = 'mysql.railway.internal';
+$user = 'root';
+$pass = 'zJECPcanVgwbtvlERXEmrtXbqgcKjXhB';
+$dbname = 'railway';
+$port = 3306;
+$secret_key = "dragonfruit42";
+$tname = 'scores';
+$score_number = '10';
 ?>
